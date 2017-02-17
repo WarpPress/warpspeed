e6a0f88f98bb38aa802ed37c67d41e1e0a6815b4
xhad <chyad@hotmail.com>
2017-02-18 04:52:10 +0900

    inline css

--- src/js/init.js
+++ src/js/init.js
@@ -2,11 +2,6 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-    
-    $(document).ready(function() {
-        $("div#ghost").removeClass("hidden");
-    });
-
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
